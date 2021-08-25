9c170da25acc19541bc6f76e59eb6ac5cfd52e02
fora-heims <foraheims@gmail.com>
2021-08-25 15:50:15 -0700

    my first app

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 // import functions
 
 // reference needed DOM elements
+const catImage = document.getElementById('cat-image');
+const dogImage = document.getElementById('dog-image');
+const horseImage = document.getElementById('horse-image');
+const catSound = document.getElementById('cat-noise');
+const dogSound = document.getElementById('dog-noise');
+const horseSound = document.getElementById('horse-noise');
 
+catImage.addEventListener('click', () => {
+  catSound.play();
+});
+
+dogImage.addEventListener('click', () => {
+  dogSound.play();
+});
+
+horseImage.addEventListener('click', () => {
+  horseSound.play();
+});
 
 // set event listeners 
   // get user input(s)
